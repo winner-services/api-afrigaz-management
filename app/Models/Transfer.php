ad65d3c8d3b47f2917a3b20df05ff6e04e1463c5
winner00kambale <vainqueurkambale@gmail.com>
2026-05-13 10:32:25 +0100

    update commit

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -27,7 +27,7 @@ class Transfer extends Model
     }
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'addedBy');
     }
     public function toBranch()
     {
