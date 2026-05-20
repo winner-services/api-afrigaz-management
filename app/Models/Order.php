c3e41580fa14da0858f55492a96769a2d50c7d14
winner00kambale <vainqueurkambale@gmail.com>
2026-05-20 15:23:42 +0100

    update commit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['distributor_id', 'reference', 'status', 'total', 'payment_method', 'delivery_address', 'confirmed_by', 'rejected_by', 'note', 'amount', 'order_date'])]
 class Order extends Model
 {
-    //
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
