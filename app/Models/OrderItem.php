5fa45020e1b0440261da50c474a8ac491ab934d2
winner00kambale <vainqueurkambale@gmail.com>
2026-05-20 15:26:38 +0100

    update commit

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 #[Fillable(['order_id', 'product_id', 'quantity', 'unit_price', 'subtotal', 'delivered_quantity'])]
 class OrderItem extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
