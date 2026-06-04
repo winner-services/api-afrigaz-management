b2203419c7152cc2e5cec5abbe524fdd68bc5dea
winner00kambale <vainqueurkambale@gmail.com>
2026-06-04 08:03:19 +0100

    sale final commit

--- app/Http/Middleware/LoginTimeMiddleware.php
+++ app/Http/Middleware/LoginTimeMiddleware.php
@@ -13,7 +13,10 @@ class LoginTimeMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (! $request->is('api/v1/auth/login')) {
+        // if (! $request->is('api/v1/auth/login')) {
+        //     return $next($request);
+        // }
+        if (! in_array($request->route()->getName(), ['login', 'loginMobile'])) {
             return $next($request);
         }
 
@@ -57,7 +60,7 @@ class LoginTimeMiddleware
         $grace = $closing
             ->copy()
             ->addMinutes($settings->grace_minutes ?? 0);
-            // dd($now->toDateTimeString(), $opening->toDateTimeString(), $closing->toDateTimeString(), $grace->toDateTimeString());
+        // dd($now->toDateTimeString(), $opening->toDateTimeString(), $closing->toDateTimeString(), $grace->toDateTimeString());
 
         if (! $now->between($opening, $grace)) {
 
