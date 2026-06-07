1dda35fd989474f73c34b677fcfbf2cbed7c74f1
winner00kambale <vainqueurkambale@gmail.com>
2026-06-07 09:50:55 +0100

    finnal commit

--- app/Http/Controllers/Api/Auth/AuthenticationController.php
+++ app/Http/Controllers/Api/Auth/AuthenticationController.php
@@ -97,7 +97,7 @@ class AuthenticationController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email'    => ['required', 'string'],
-            'password' => ['required', 'string', 'min:4'],
+            'password' => ['required', 'string'],
         ]);
 
         if ($validator->fails()) {
